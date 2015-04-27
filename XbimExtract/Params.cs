d75116f8b2778499abe65978c0ff422492af72f2
Richard Munn <richard.munn@viewpoint.com>
2015-04-27 10:41:04 +0100

    Imported XbimExtract from the old Codeplex repo

---
+++ XbimExtract/Params.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Xbim.XbimExtensions.Interfaces;
+
+namespace XbimExtract
+{
+    public class Params
+    {
+        public string SourceModelName;
+        public string TargetModelName;
+        public List<int> EntityLabels;
+        public bool IsValid { get; set; }
+        public bool SourceIsXbimFile { get; set; }
+        /// <summary>
+        /// Include project and other context objects to create a vlaid schema (ish)
+        /// </summary>
+        public bool IncludeContext { get; set; }
+
+
+        public static Params ParseParams(string[] args)
+        {
+            Params result = new Params(args);
+
+            return result;
+        }
+
+        private Params(string[] args)
+        {
+
+            try
+            {
+                if (args.Length < 3) throw new Exception("Invalid number of Parameters, 3 required");
+                SourceModelName = GetModelFileName(args[0], ".ifc");
+                if (!File.Exists(SourceModelName)) throw new Exception(SourceModelName + " does not exist");
+                TargetModelName = GetModelFileName(args[1], ".xbim");
+                SourceIsXbimFile = Path.GetExtension(SourceModelName).ToLower() == ".xbim";
+                EntityLabels = new List<int>(args.Length - 2);
+                for (int i = 2; i < args.Length; i++) EntityLabels.Add(Int32.Parse(args[i]));
+                // Parameters are valid
+                IsValid = true;
+                IncludeContext = true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("XbimExtract SourceModelName TargetModelName 325 [1756 2678]");
+                Console.WriteLine("\tModelName extensions supported are .xBIM, .ifc, .ifcxml");
+                IsValid = false;
+            }
+        }
+
+        private string GetModelFileName(string arg, string defaultExtension)
+        {
+            string extName = Path.GetExtension(arg);
+            string fileName = Path.GetFileNameWithoutExtension(arg);
+            string dirName = Path.GetDirectoryName(arg);
+            if (string.IsNullOrWhiteSpace(dirName))
+                dirName = Directory.GetCurrentDirectory();
+            if (string.IsNullOrWhiteSpace(extName))
+                extName = defaultExtension;
+            switch (extName.ToLower())
+            {
+                case ".xbim":
+                case ".ifc":
+                case ".ifcxml":
+                    return Path.ChangeExtension(Path.Combine(dirName, fileName), extName);
+                default:
+                    throw new Exception("Invalid file extension (" + extName + ")");
+            }
+           
+        }
+
+
+       
+    }
+
+}
---
+++ XbimExtract/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xbim.Ifc2x3.Kernel;
+using Xbim.Ifc2x3.ProductExtension;
+using Xbim.IO;
+using Xbim.XbimExtensions.Interfaces;
+
+namespace XbimExtract
+{
+    class Program
+    {
+        /// <summary>
+        /// Given a list of IFC entity labels in the source model, extracts them and inserts them in the target model
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Params arguments = Params.ParseParams(args);
+            if (arguments.IsValid)
+            {
+                try
+                {
+                    ReportProgressDelegate progDelegate = delegate(int percentProgress, object userState)
+                    {
+                        Console.Write(string.Format("{0:D5}", percentProgress));
+                        ResetCursor(Console.CursorTop); 
+                    };
+                    using (XbimModel source = new XbimModel())
+                    {
+                        Console.WriteLine(string.Format("Reading {0}", arguments.SourceModelName));
+                        if (arguments.SourceIsXbimFile)
+                            source.Open(arguments.SourceModelName);
+                        else
+                            source.CreateFrom(arguments.SourceModelName, null, progDelegate,true);
+                        Console.WriteLine();
+                        Console.WriteLine("Extracting and copying to " + arguments.TargetModelName);
+                        using (XbimModel target = XbimModel.CreateTemporaryModel())
+                        {
+                            if (arguments.IncludeContext) //add in the project and building to maintain a valid-ish file
+                            {
+                                IfcProject project = source.IfcProject; //get the spatial decomposition hierarchy
+                               
+                                foreach (var rel in project.IsDecomposedBy)
+                                {
+                                    arguments.EntityLabels.Add(rel.EntityLabel);
+                                }
+                                
+                            }
+                            XbimInstanceHandleMap maps = new XbimInstanceHandleMap(source, target); //prevents the same thing being written twice
+                            using (XbimReadWriteTransaction txn = target.BeginTransaction())
+                            {
+                                foreach (var label in arguments.EntityLabels)
+                                {
+                                    IPersistIfcEntity ent = source.Instances[label];
+                                    target.InsertCopy(ent, maps, txn, false);
+                                }  
+                                txn.Commit();
+                            }
+                            File.Delete(arguments.TargetModelName);
+                            Console.WriteLine("Saving to " + arguments.TargetModelName);
+                            target.SaveAs(arguments.TargetModelName,null,progDelegate);
+                            Console.WriteLine("Success");
+                        }
+
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine(); //wait for use to kill
+        }
+
+        private static void ResetCursor(int top)
+        {
+            try
+            {
+                // Can't reset outside of buffer, and should ignore when in quiet mode
+                if (top >= Console.BufferHeight)
+                    return;
+                Console.SetCursorPosition(0, top);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
